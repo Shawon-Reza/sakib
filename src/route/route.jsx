17982dad908ccb925ed9579214ffe0b58f9f8caf
Shawon Reza <shawon.reza.sr81@gmail.com>
2026-02-19 10:33:46 +0600

    Initialize project

---
+++ src/route/route.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-2xl">Hello World</div>,
+  },
+]);
+
+
+
+export default router;
